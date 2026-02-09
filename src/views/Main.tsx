b5ad152e9a4719122029faee825e154fa99eaabd
suhaibh27 <40864369+suhaibh27@users.noreply.github.com>
2026-02-09 16:53:30 +0300

    set-up and template
    main template and tailwind setup

---
+++ src/views/Main.tsx
@@ -0,0 +1,28 @@
+function Main() {
+    return (
+        <div className="flex flex-col min-h-screen  bg-[color:var(--primary-color)]">
+            <div className="p-4 flex justify-between items-center">
+                <h1 className="text-xl text-white">Route Planning Tool</h1>
+                <div className="flex items-center">
+                    <button className="p-3 bg-gray-300 rounded-sm mr-4">optimize</button>
+                    <button className="p-3 bg-gray-300 rounded-sm">export</button>
+                </div>
+            </div>
+            <div className="flex flex-1">
+                <div className="w-64 md:w-80 lg:w-96 p-4 flex flex-col gap-10">
+                    <div className="border rounded-sm border-[color:var(--secondary-color)] p-4">
+
+                    </div>
+                </div>
+                <div className="flex-1 p-4 bg-gray-300">
+                    mapbox
+                </div>
+            </div>
+            <div className="p-4 ">
+                footer
+            </div>
+        </div>
+    )
+}
+
+export default Main
